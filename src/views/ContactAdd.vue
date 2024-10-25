044ce544d64d8c4f4b7722676868dadd7d83710f
nhan295 <tranthihongnhan295@gmail.com>
2024-10-25 12:39:31 +0700

    create add contact page

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+import ContactForm from '@/components/ContactForm.vue';
+import contactsService from '@/services/contacts.service';
+
+const contact = ref({
+    name: '',
+    email: '',
+    address: '',
+    phone: '',
+    favorite: 0,
+    avatar: null,
+});
+const message = ref('')
+
+async function addContact(contact) {
+    try {
+        await contactsService.createContact(contact)
+        message.value = 'Liên hệ được thêm thành công!';
+    }
+    catch {
+        message.value = 'Lỗi khi thêm liên hệ';
+    }
+}
+</script>
+
+<template>
+    <div v-if="contact" class="page">
+    <h4>Thêm liên hệ</h4>
+    <ContactForm
+      :contact="contact"
+      @submit:contact="addContact"
+    />
+    <p>{{ message }}</p>
+  </div>
+</template>
